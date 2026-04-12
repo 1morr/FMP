b0ea550cb0404f2bcf4311f3b7fe2e4e109adb04
imoR <ivanspwong@gmail.com>
2026-04-12 14:03:59 +0800

    chore: update .gitignore to include graphify-out and modify AndroidManifest.xml to change launchMode to singleTask; enhance main.cpp for single instance handling and multi-window support

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -6,6 +6,37 @@
 #include "flutter_window.h"
 #include "utils.h"
 
+namespace {
+
+constexpr wchar_t kMainWindowClassName[] = L"FLUTTER_RUNNER_WIN32_WINDOW";
+constexpr wchar_t kMainWindowTitle[] = L"FMP - Flutter Music Player";
+constexpr wchar_t kSingleInstanceMutexName[] = L"Local\\FMP_MainInstance";
+
+bool IsMultiWindowLaunch(const std::vector<std::string>& arguments) {
+  return !arguments.empty() && arguments.front() == "multi_window";
+}
+
+void ActivateExistingInstance() {
+  HWND existing_window =
+      ::FindWindowW(kMainWindowClassName, kMainWindowTitle);
+  if (existing_window == nullptr) {
+    existing_window = ::FindWindowW(nullptr, kMainWindowTitle);
+  }
+  if (existing_window == nullptr) {
+    return;
+  }
+
+  if (!::IsWindowVisible(existing_window)) {
+    ::ShowWindow(existing_window, SW_SHOW);
+  }
+  if (::IsIconic(existing_window)) {
+    ::ShowWindow(existing_window, SW_RESTORE);
+  }
+  ::SetForegroundWindow(existing_window);
+}
+
+}  // namespace
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   // Set AppUserModelID for SMTC (System Media Transport Controls) identity.
@@ -21,17 +52,32 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
+
+  HANDLE single_instance_mutex = nullptr;
+  if (!IsMultiWindowLaunch(command_line_arguments)) {
+    single_instance_mutex =
+        ::CreateMutexW(nullptr, FALSE, kSingleInstanceMutexName);
+    if (single_instance_mutex != nullptr &&
+        ::GetLastError() == ERROR_ALREADY_EXISTS) {
+      ActivateExistingInstance();
+      ::CloseHandle(single_instance_mutex);
+      ::CoUninitialize();
+      return EXIT_SUCCESS;
+    }
+  }
+
   flutter::DartProject project(L"data");
-
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
-
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"FMP - Flutter Music Player", origin, size)) {
+  if (!window.Create(kMainWindowTitle, origin, size)) {
+    if (single_instance_mutex != nullptr) {
+      ::CloseHandle(single_instance_mutex);
+    }
+    ::CoUninitialize();
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
@@ -42,6 +88,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
     ::DispatchMessage(&msg);
   }
 
+  if (single_instance_mutex != nullptr) {
+    ::CloseHandle(single_instance_mutex);
+  }
   ::CoUninitialize();
   return EXIT_SUCCESS;
 }
